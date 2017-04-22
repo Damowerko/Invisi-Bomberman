2a885883dbcee7f45a298a161d84df00223168f1
Damian Owerko <damowerko@gmail.com>
2017-04-22 14:54:07 -0400

    Main server file.

--- src/Server/ServerMain.java
+++ src/Server/ServerMain.java
@@ -1,16 +1,16 @@
-import java.util.Timer;
-import java.util.TimerTask;
+package Server;
 
 /**
- * This methods initializes the server model and network sockets.
+ * This methods initializes the server model and backend (in a separate thread).
  */
 public class ServerMain {
 
+    private ServerMain() {
+    }
+
     public static void main(String[] args){
         final ServerModel serverModel = new ServerModel();
-        final ServerBackend server = new ServerBackend();
+        final ServerBackend server = new ServerBackend(serverModel);
         new Thread(server).start();
-    }
-    private ServerMain{
     }
 }
